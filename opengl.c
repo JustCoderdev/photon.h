98b5401d8e5a50467648658223c09b4d6fd0b251
JustCoderdev <107036402+JustCoderdev@users.noreply.github.com>
2025-03-09 12:21:17 +0100

    Added heart

--- opengl.c
+++ opengl.c
@@ -1,153 +1,171 @@
-#define _POSIX_C_SOURCE 199309L
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <assert.h>
+#include <math.h>
 
 /* OpenGL 1.1 spec <https://registry.khronos.org/OpenGL/specs/gl/glspec11.pdf> */
 #include <GL/gl.h>
-#include <GL/glx.h>
+#include <GLFW/glfw3.h>
 
-/* XLib docs <https://www.x.org/releases/X11R7.7/doc/libX11/libX11/libX11.html#Introduction_to_Xlib> */
-#include <X11/Xlib.h>
+typedef unsigned char     n8;
+typedef unsigned short    n16;
+typedef unsigned int      n32;
+typedef unsigned long int n64;
 
-unsigned long ec = 0;
+typedef signed char     i8;
+typedef signed short    i16;
+typedef signed int      i32;
+typedef signed long int i64;
 
-#define PRINT_GL_ERRORS() do { \
-			GLenum e; \
-			do { \
-				e = glGetError(); \
-				switch(e) \
-				{ \
-					case GL_INVALID_ENUM: \
-						printf("[%lu] GLERROR:%s:%d: INVALID_ENUM\n", ec++, __FILE__, __LINE__); \
-						break; \
-					case GL_INVALID_VALUE: \
-						printf("[%lu] GLERROR:%s:%d: INVALID_VALUE\n", ec++, __FILE__, __LINE__); \
-						break; \
-					case GL_INVALID_OPERATION: \
-						printf("[%lu] GLERROR:%s:%d: INVALID_OPERATION\n", ec++, __FILE__, __LINE__); \
-						break; \
-					case GL_STACK_OVERFLOW: \
-						printf("[%lu] GLERROR:%s:%d: STACK_OVERFLOW\n", ec++, __FILE__, __LINE__); \
-						break; \
-					case GL_STACK_UNDERFLOW: \
-						printf("[%lu] GLERROR:%s:%d: STACK_UNDERFLOW\n", ec++, __FILE__, __LINE__); \
-						break; \
-					case GL_OUT_OF_MEMORY: \
-						printf("[%lu] GLERROR:%s:%d: OUT_OF_MEMORY\n", ec++, __FILE__, __LINE__); \
-						exit(1); \
-				} \
-			} \
-			while(e != GL_NO_ERROR); \
-		} while(0)
+typedef enum {
+	true  = (1 == 1),
+	false = (1 != 1)
+} bool;
+
+typedef enum {
+	success = false,
+	failure = true
+} error;
+
+typedef int Errno;
+
+typedef struct {
+	float x, y;
+} Point;
+
+typedef struct {
+	int width, height;
+} Size;
+
+#define PI 3.1415926535f
+
+/* -1..1 -> 0..1 */
+#define RAD_TO_RAT(RAD) (float)(((RAD) + 1) / 2)
+/* 0..1 -> -1..1 */
+#define RAT_TO_RAD(RAT) (float)(((RAT) - 0.5f ) * 2)
+
+#define PRINT_GL_ERRORS() glPrintErrors(__FILE__, __LINE__)
+static void glPrintErrors(char* file, int line)
+{
+	static unsigned long ec = 0;
+
+	char* msg = "NO_ERROR";
+	GLenum e;
+
+	do {
+		switch(e = glGetError())
+		{
+			case GL_NO_ERROR: return;
+			case GL_INVALID_ENUM:      msg = "INVALID_ENUM";      break;
+			case GL_INVALID_VALUE:     msg = "INVALID_VALUE";     break;
+			case GL_INVALID_OPERATION: msg = "INVALID_OPERATION"; break;
+			case GL_STACK_OVERFLOW:    msg = "STACK_OVERFLOW";    break;
+			case GL_STACK_UNDERFLOW:   msg = "STACK_UNDERFLOW";   break;
+			case GL_OUT_OF_MEMORY:     msg = "OUT_OF_MEMORY";     exit(failure);
+			default: assert(0 && "UNREACHABLE");
+		}
+		printf("[%lu] GLERROR:%s:%d: %s\n", ec++, file, line, msg);
+	}
+	while(e != GL_NO_ERROR);
+}
+
+
+static void handle_inputs(GLFWwindow *window, Size* viewport, Point* cursor_pos)
+{
+	double cx, cy;
+	int vw = viewport->width,
+		vh = viewport->height;
+
+	/* Update cursor */
+	glfwGetCursorPos(window, &cx, &cy);
+	cursor_pos->x = RAT_TO_RAD(cx / vw);
+	cursor_pos->y = -RAT_TO_RAD(cy / vh);
+
+	/* Update keyboard */
+	if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+}
+
+static void draw_gay_circle(float x, float y, int steps, float aspect_ratio, float size)
+{
+	int i;
+
+	glBegin(GL_TRIANGLE_FAN);
+		glColor3f(0.0f, 1.0f, 0.0f);
+		glVertex3f(x, y, 0.0f);
+
+		for(i = 0; i <= steps; ++i)
+		{
+			double deg = ((float)i / (float)steps) * 2 * PI;
+			glColor3f(RAD_TO_RAT(sin(deg)), 0.0f, RAD_TO_RAT(cos(deg)));
+			glVertex3f(x + (float)cos(deg) * size, y + (float)sin(deg) * size * aspect_ratio, 0.0f);
+		}
+	glEnd();
+	PRINT_GL_ERRORS();
+}
+
+static void draw_flat_circle(float x, float y, int steps, float aspect_ratio, float size, float r, float g, float b)
+{
+	int i;
+
+	glBegin(GL_TRIANGLE_FAN);
+		glColor3f(r, g, b);
+		glVertex3f(x, y, 0.0f);
+
+		for(i = 0; i <= steps; ++i)
+		{
+			double deg = ((float)i / (float)steps) * 2 * PI;
+			/* glColor3f(RAD_TO_RAT(sin(deg)), 0.0f, RAD_TO_RAT(cos(deg))); */
+			glVertex3f(x + (float)cos(deg) * size, y + (float)sin(deg) * size * aspect_ratio, 0.0f);
+		}
+	glEnd();
+	PRINT_GL_ERRORS();
+}
+
+#define SCREEN_WIDTH 600
+#define SCREEN_HEIGHT 400
 
 /* Creating a window X11 <https://hereket.com/posts/linux_creating_x11_windows> */
 int main(void) {
-	int is_window_alive = 1;
+	float aspect_ratio = 0;
+	Size viewport = {0};
+	Point cursor_pos = {0};
+	int i;
 
-	struct timespec now, old;
-	time_t dt;
+	GLFWwindow* window;
 
-	struct {
-		GLfloat x, y;
-	} cursor_pos = {0, 0};
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 1);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_ANY_PROFILE);
 
-	struct {
-		int width, height;
-	} viewport_size = {1, 1};
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+	glfwWindowHint(GLFW_FLOATING, GLFW_TRUE);
 
-	XVisualInfo* visual_info;
-	GLint visual_attr[] = { GLX_RGBA, GLX_DEPTH_SIZE, 24, GLX_DOUBLEBUFFER, None };
-	GLXContext gl_ctx;
-
-	XSetWindowAttributes set_window_attr;
-	Window root_w, window;
-
-	Display* display = XOpenDisplay(NULL);
-	if(display == NULL)
+	window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Emulator", NULL, NULL);
+	if(window == NULL)
 	{
-		fprintf(stderr, "ERROR:%s:%d: Couldn't connect to X server\n", __FILE__, __LINE__);
-		exit(1);
+		fprintf(stderr, "ERROR:%s:%d: Failed to create window\n", __FILE__, __LINE__);
+		glfwTerminate();
+		exit(failure);
 	}
 
-	root_w = DefaultRootWindow(display);
+	glfwMakeContextCurrent(window);
+	printf("OpenGL Version %s\n", glGetString(GL_VERSION));
 
-	visual_info = glXChooseVisual(display, 0, visual_attr);
-	if(visual_info == NULL)
-	{
-		fprintf(stderr, "ERROR:%s:%d: Appropriate visuals not found\n", __FILE__, __LINE__);
-		exit(1);
-	}
+	glfwGetFramebufferSize(window, &viewport.width, &viewport.height);
+	aspect_ratio = (float)viewport.width / (float)viewport.height;
+	printf("Viewport %dx%d (%.2f)\n", viewport.width, viewport.height, aspect_ratio);
 
-	set_window_attr.colormap = XCreateColormap(display, root_w, visual_info->visual, AllocNone);
-	set_window_attr.event_mask = ExposureMask | KeyPressMask | PointerMotionMask;
-	set_window_attr.background_pixel = 0xFF181818;
-
-	window = XCreateWindow(display, root_w, 0, 0, 600, 400, 0,
-			visual_info->depth, InputOutput, visual_info->visual,
-			CWColormap | CWEventMask, &set_window_attr);
-
-	XMapWindow(display, window);
-	XStoreName(display, window, "awawa");
-
-	gl_ctx = glXCreateContext(display, visual_info, NULL, GL_TRUE);
-	glXMakeCurrent(display, window, gl_ctx);
-
-	glEnable(GL_DEPTH_TEST);
+	glViewport(0, 0, viewport.width, viewport.height);
 	PRINT_GL_ERRORS();
 
-	assert(clock_gettime(CLOCK_MONOTONIC, &old) == 0);
-	while(is_window_alive) {
-		XEvent event;
-		XNextEvent(display, &event);
-
-		assert(clock_gettime(CLOCK_MONOTONIC, &now) == 0);
-		dt = now.tv_nsec - old.tv_nsec;
-		old = now;
-
-#define DTtoHZ(DT) (1/((double)(DT)/1e9))
-		printf("DeltaTime %lu - HZ %f\n", dt, DTtoHZ(dt));
-
-		switch(event.type) {
-			case Expose:
-			{
-				XWindowAttributes window_attr;
-
-				/* XExposeEvent* expose_event = (XExposeEvent *)&event; */
-				/* if(expose_event->count > 0) break; */
-
-				XGetWindowAttributes(display, window, &window_attr);
-				glViewport(0, 0, window_attr.width, window_attr.height);
-				PRINT_GL_ERRORS();
-
-				viewport_size.width = window_attr.width;
-				viewport_size.height = window_attr.height;
-				break;
-			}
-
-			case MotionNotify:
-			{
-				XMotionEvent* motion_event = (XMotionEvent *)&event;
-
-				cursor_pos.x = ( ( (GLfloat)motion_event->x / (GLfloat)viewport_size.width  ) - 0.5f ) *  2;
-				cursor_pos.y = ( ( (GLfloat)motion_event->y / (GLfloat)viewport_size.height ) - 0.5f ) * -2;
-				/* printf("MOUSE: RAW %dx%d - PROCESSED %.2fx%.2f\n", motion_event->x, motion_event->y, cursor_pos.x, cursor_pos.y); */
-				break;
-			}
-
-			case KeyPress:
-			{
-				XKeyPressedEvent* key_event = (XKeyPressedEvent *)&event;
-				if(key_event->keycode == XKeysymToKeycode(display, XK_Escape))
-				{
-					is_window_alive = 0;
-				}
-				break;
-			}
-		}
-
+	while(!glfwWindowShouldClose(window))
+	{
+		handle_inputs(window, &viewport, &cursor_pos);
+		/* -------------------- */
 
 		glClearColor(0.094f, 0.094f, 0.094f, 1.0);
 		PRINT_GL_ERRORS();
@@ -155,37 +173,54 @@ int main(void) {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		PRINT_GL_ERRORS();
 
-		glBegin(GL_TRIANGLES);
-			glColor3f(1.0f, 0.0f, 0.0f); glVertex3f(  0.0f,  0.5f, 0.0f);
-			glColor3f(0.0f, 1.0f, 0.0f); glVertex3f( -0.5f, -0.5f, 0.0f);
-			glColor3f(0.0f, 0.0f, 1.0f); glVertex3f( +0.5f, -0.5f, 0.0f);
-		glEnd();
+		/* glBegin(GL_TRIANGLES); */
+		/* 	glColor3f(1.0f, 0.0f, 0.0f); glVertex3f(  0.0f,  0.5f, 0.0f); */
+		/* 	glColor3f(0.0f, 1.0f, 0.0f); glVertex3f( -0.5f, -0.5f, 0.0f); */
+		/* 	glColor3f(0.0f, 0.0f, 1.0f); glVertex3f( +0.5f, -0.5f, 0.0f); */
+		/* glEnd(); */
+		/* PRINT_GL_ERRORS(); */
 
 		glPointSize(10.0f);
 		PRINT_GL_ERRORS();
 
 		glBegin(GL_POINTS);
-			glColor3f(1.0f, 0.0f, 0.0f); glVertex3f(-1.0f, -0.5f, 0.0f);
-			glColor3f(1.0f, 0.0f, 0.0f); glVertex3f( 0.0f,  0.0f, 0.0f);
-			glColor3f(1.0f, 0.0f, 0.0f); glVertex3f( 1.0f,  0.5f, 0.0f);
-
-			glColor3f(0.0f, 1.0f, 0.0f); glVertex3f(cursor_pos.x, cursor_pos.y, 0.0f);
+			glColor3f(1.0f, 0.0f, 0.0f);
+			glVertex3f(cursor_pos.x, cursor_pos.y, 0.0f);
 		glEnd();
 		PRINT_GL_ERRORS();
 
-		printf("MOUSE %.2fx%.2f\n", cursor_pos.x, cursor_pos.y);
+		{
+			/* float x = cursor_pos.x, */
+			/* 	  y = cursor_pos.y; */
+			float size = 0.2f;
+			float x = 0.0f,
+				  y = 0.0f;
+			float xoff = 0.15f,
+				  yoff = 0.5f;
+			float r = 1.00f,
+				  g = 0.0f,
+				  b = 0.0f;
 
-		glXSwapBuffers(display, window);
-		PRINT_GL_ERRORS();
+			draw_gay_circle(x - xoff, y + yoff, 16, aspect_ratio, size);
+			draw_gay_circle(x + xoff, y + yoff, 16, aspect_ratio, size);
 
-		XFlush(display);
+			/* glBegin(GL_TRIANGLES); */
+			/* 	glColor3f(r, g, b); glVertex3f(x - xoff - size, y + yoff - 0.1f, 0.0f); */
+			/* 	glColor3f(r, g, b); glVertex3f(x + xoff + size, y + yoff - 0.1f, 0.0f); */
+			/* 	glColor3f(r, g, b); glVertex3f(0.0f, -0.3f, 0.0f); */
+			/* glEnd(); */
+
+			/* draw_flat_circle(x - xoff, y + yoff, 16, aspect_ratio, size, r, g, b); */
+			/* draw_flat_circle(x + xoff, y + yoff, 16, aspect_ratio, size, r, g, b); */
+
+			(void)i;
+		}
+
+		/* -------------------- */
+		glfwSwapBuffers(window);
+		glfwPollEvents();
 	}
 
-	glXMakeCurrent(display, None, NULL);
-	glXDestroyContext(display, gl_ctx);
-
-	XDestroyWindow(display, window);
-	XCloseDisplay(display);
-
-	return 0;
+	glfwTerminate();
+	return success;
 }
